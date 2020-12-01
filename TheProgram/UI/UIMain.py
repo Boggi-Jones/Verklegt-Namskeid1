562f4a889a5106e559e1392cfd291b08479d75b0
Jonsteinar <jonss20@ru.is>
2020-12-01 10:11:41 +0000

    UIMain

---
+++ TheProgram/UI/UIMain.py
@@ -0,0 +1,11 @@
+from UI.ContractUI import ContractUI
+from UI.EmployeeUI import EmployeeUI
+from UI.LocationUI import LocationUI
+from UI.FleetUI import FleetUI
+
+class UIMain():
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
