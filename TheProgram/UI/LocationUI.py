cf3e3966c1040ad15481461c948e35072cb20c70
Jonsteinar <jonss20@ru.is>
2020-12-01 20:35:14 +0000

    ds

--- TheProgram/UI/LocationUI.py
+++ TheProgram/UI/LocationUI.py
@@ -1,6 +1,36 @@
+from UI.UIMain import UIMain
+from Logic.LogicMain import LogicMain
+
 class UILocation():
     def __init__(self):
-        pass
+        self.logic = LogicMain()
+        self.ui = UIMain()
 
-    def __str__(self):
-        pass
+    def location_loop(self):
+        choice = input("""# -------Rental Location information ---------
+# 1. Edit rental location
+# 2. All locations
+#
+#
+#
+#
+#
+# <- Back
+# Choice:__
+# --------------------------------------------""")
+
+    if choice == "1":
+        edit_location()
+    elif choice == "2":
+        all_locations()
+    else:
+        print("Invalid choice!")
+
+    def edit_location():
+
+
+    def all_locations():
+        results = self.logic.all_locations()
+        print("\nAll locations: ")
+        for location in results:
+            print(location)
