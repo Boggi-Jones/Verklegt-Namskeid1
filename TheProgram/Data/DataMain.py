b305f0e827741eac61a3415846d973891f5609a0
unknown <skarpi.isak@gmail.com>
2020-12-01 16:20:24 +0000

    Endir dagsins allir happy

--- TheProgram/Data/DataMain.py
+++ TheProgram/Data/DataMain.py
@@ -1,12 +1,13 @@
 import csv
+from Models.Employee import Employee
 class DataMain():
     def get_list(self, filename):
         retList = []
         with open(f"Data/data/{filename}.csv", newline="", encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                if value == "Employee":
-                    emp = Employee(row["name"], row["ssn"], row["adress"], row["home_phone"], row["gsm_phone"], row["email"], row["location"], row["role"])
+                if filename == "Employee":
+                    emp = Employee(row["name"], row["ssn"], row["address"], row["home_phone"], row["gsm_phone"], row["email"], row["location"], row["role"])
                     retList.append(emp)
         return retList
     def add_to_list(self, filename, value):
--- TheProgram/main.py
+++ TheProgram/main.py
@@ -1,3 +1,3 @@
 from UI.UIMain import UIMain
 
-UIMain()
+UIMain().UILoop()
