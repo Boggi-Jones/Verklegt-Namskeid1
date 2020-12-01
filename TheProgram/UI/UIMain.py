473bfbbdaf577672bd2d22965c58fa90354a1742
Andri Freysson <andrifreys@Andris-MacBook-Pro.local>
2020-12-01 12:24:00 +0000

    Firsta UI push

--- TheProgram/UI/UIMain.py
+++ TheProgram/UI/UIMain.py
@@ -5,7 +5,39 @@ from UI.FleetUI import FleetUI
 
 class UIMain():
     def __init__(self):
-        pass
+        print("Welcome to NaN Air")
+        self.Logic = LogicMain()
+        self.UILoop() 
 
-    def __str__(self):
-        pass
+    def UILoop(self):
+        while True:
+            choice = Printmenu()
+            if choice == "1":
+                EmployeeUI()
+            elif choice == "2":
+                FleetUI()
+            elif choice == "3":
+                LocationUI()
+            elif choice == "4":
+                ContractUI()
+            elif choice == "5":
+                pass            # Þurfum að bæta við klasa
+            elif choice == "q":
+                break
+            else:
+                print("Invalid entry")
+
+    def Printmenu(self):
+        MENU = print('''# Veldu nr. til að fara áfram í valmynd 
+# Veldu "q" til að hætta
+#
+# 1. Employee accounts
+# 2. Manage vehicle fleet
+# 3. Rental Locations
+# 4. Contracts
+# 5. Business reports
+# Q. Quit
+--------------------------------------------
+''')
+        choice = input("choice: ")
+        return choice
