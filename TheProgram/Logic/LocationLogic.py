f232d696a45a4334ba3e14f5f5a5cad1ef78c378
Solvismara <34425392+Solvismara@users.noreply.github.com>
2020-12-02 11:02:01 +0000

    Update LocationLogic.py
    Updated Location logic

--- TheProgram/Logic/LocationLogic.py
+++ TheProgram/Logic/LocationLogic.py
@@ -1,13 +1,28 @@
 from Data.DataMain import DataMain
 class LocationLogic:
-    def filtercountry(self, attribute, filter):
-        pass
+    def __init__(self):
+        self.datamain = DataMain()
+        self.position = "Location"
 
-    def addlocation(self, country, airport, phone, hours):
-        pass
+    def filtercountry(self, filter_or_id, attribute):
+        list_of_locations = self.datamain.getlist(self.position)
+        retlist = []
+        if filter_or_id != None:
+            for location in list_of_locations:
+                if location.__getattribute__(attribute) == filter_or_id:
+                    retlist.append(location)
+            return retlist
+        else:
+            return list_of_locations
 
-    def removelocation(self):
-        pass
+    def addlocation(self, new_information):
+        self.datamain.add_to_list(self.position, new_information)
+
+    def removelocation(self, filter_or_id):
+        self.datamain.removeitem(self.position, filter_or_id)
     
-    def editlocationinfo(self, airport_name):
-        pass
+    def editlocationinfo(self, filter_or_id, attribute, new_information):
+        single_location = self.datamain.get_one_item(self.position, filter_or_id)
+        single_location.__setattr__(attribute, new_information)
+        self.datamain.removeitem(self.position, filter_or_id)
+        self.datamain.add_to_list(self.position, single_location)
