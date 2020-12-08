d4d2be35046b8cf76d8a936b143965442ce05d43
Andri Freysson <andrifreys@Andris-MacBook-Pro.local>
2020-12-08 12:02:29 +0000

    UI employee

--- TheProgram/UI/EmployeeUI.py
+++ TheProgram/UI/EmployeeUI.py
@@ -91,12 +91,27 @@ class EmployeeUI():
  -----------------------------------------------------------------------------''')
         name = input(" | Name: ")
         ssn = input(" | SSN: ")
+        while self.logic.input_checking(1, ssn) == False:
+            print(" | SSN must be in the correct format: '123456-1234' ")
+            ssn = input(" | SSN: ")
         address = input(" | Address: ")
         home_phone = input(" | Home phone: ")
+        while self.logic.input_checking(2, home_phone) == False:
+            print(" | Home phone must be in the correct format: '1234567' ")
+            home_phone = input(" | Home phone: ")
         smart_phone = input(" | Smart phone: ")
+        while self.logic.input_checking(2, smart_phone) == False:
+            print(" | Smart phone must be in the correct format: '1234567' ")
+            smart_phone = input(" | Smart phone: ")
         email = input(" | Email: ")
+        while self.logic.input_checking(0, email) == False:
+            print(" | Email must be in the correct format: 'name@name.is' ")
+            email = input(" | Email: ")
         location = input(" | Location: ")
         role = input(" | Company role: ")
+        while self.logic.input_checking(3, role) == False:
+            print(" | company role is either 'ceo', 'fleet' or 'base'!")
+            role = input(" | Company role: ")
         the_employee = Employee(name, ssn, address, home_phone, smart_phone, email, location, role)
 
         print('''\n -----------------------------------------------------------------------------
@@ -200,60 +215,83 @@ class EmployeeUI():
     def update_employee(self):
         while True:
             find_employee = input(" | Enter employee SSN: ")
+            while self.logic.input_checking(1, find_employee) == False:
+                print(" | SSN must be in the correct format: '123456-1234' ")
+                find_employee = input(" | SSN: ")
             the_employee = self.logic.employee(0, find_employee, "ssn", None)
             attribute = input('''\n -----------------------------------------------------------------------------
  | -> -> Manage employee -> Update employee                                  |
  -----------------------------------------------------------------------------
  | "Select attribute you would like to change"                               |
  |                                                                           |
- | 1. Address:           {}                                                  |
- | 2. Home phone:        {}                                                  |
- | 3. Smart phone:       {}                                                  |
- | 4. Email:             {}                                                  |
- | 5. Location:          {}                                                  |
- | 6. Company role:      {}                                                  |
+ | 1. Address:           {:52s}|
+ | 2. Home phone:        {:52s}|
+ | 3. Smart phone:       {:52s}|
+ | 4. Email:             {:52s}|
+ | 5. Location:          {:52s}|
+ | 6. Company role:      {:52s}|
  |                                                                           |
  |                                                                           |
  |                                                                           |
  |                                                                           |
  -----------------------------------------------------------------------------
  | Choice: '''.format(the_employee[0].address, the_employee[0].home_phone, the_employee[0].gsm_phone, the_employee[0].email, the_employee[0].location, the_employee[0].role))
-
+            new_employee_info = "0"
             if attribute == "1":
                 attribute = "address"
+                new_employee_info = input(" | Enter new information: ")
+
             elif attribute == "2":
                 attribute = "home_phone"
+                new_employee_info = input(" | Enter new information: ")
+                while self.logic.input_checking(2, new_employee_info) == False:
+                    print(" | Home phone must be in the correct format: '1234567' ")
+
             elif attribute == "3":
                 attribute = "gsm_phone"
+                new_employee_info = input(" | Enter new information: ")
+                while self.logic.input_checking(2, new_employee_info) == False:
+                    print(" | Home phone must be in the correct format: '1234567' ")
+
             elif attribute == "4":
                 attribute = "email"
+                new_employee_info = input(" | Enter new information: ")
+                while self.logic.input_checking(0, new_employee_info) == False:
+                    print(" | Email must be in the correct format: 'name@name.is' ")
+
             elif attribute == "5":
                 attribute = "location"
+                new_employee_info = input(" | Enter new information: ")
+
             elif attribute == "6":
                 attribute = "role"
+                new_employee_info = input(" | Enter new information: ")
+                while self.logic.input_checking(3, new_employee_info) == False:
+                    print(" | company role is either 'ceo', 'fleet' or 'base'!")
+
             else:
                 print("Wrong input")
                 continue
-            new_employee_info = input(" | Enter new information: ")
+
+            #new_employee_info = input(" | Enter new information: ")
             updated_employee = self.logic.employee(3, find_employee, attribute,  new_employee_info)
             print('''\n -----------------------------------------------------------------------------
  | -> -> Manage employee -> Update employee                                  |
  -----------------------------------------------------------------------------
  | "Updated employee information"                                            |
  |                                                                           |
- | 1. Address:             {:31s}|
- | 2. Home phone:          {:31s}|
- | 3. Smart phone:         {:31s}|
- | 4. Email:               {:31s}|
- | 5. Location:            {:31s}|
- | 6. Company role:        {:31s}|
+ | 1. Address:             {:50s}|
+ | 2. Home phone:          {:50s}|
+ | 3. Smart phone:         {:50s}|
+ | 4. Email:               {:50s}|
+ | 5. Location:            {:50s}|
+ | 6. Company role:        {:50s}|
  |                                                                           |
  |                                                                           |
  |                                                                           |
  |                                                                           |
- -----------------------------------------------------------------------------
- |'''.format(updated_employee.address, updated_employee.home_phone, updated_employee.gsm_phone, updated_employee.email, updated_employee.location, updated_employee.role))
-            input("Press 'Enter' to continue")
+ -----------------------------------------------------------------------------'''.format(updated_employee.address, updated_employee.home_phone, updated_employee.gsm_phone, updated_employee.email, updated_employee.location, updated_employee.role))
+            input(" | Press 'Enter' to continue")
             break
 
     def get_all_employees(self):
@@ -270,21 +308,24 @@ class EmployeeUI():
 
     def search_employee(self):
         employee = input(" | Enter employee SSN: ")
+        while self.logic.input_checking(1, employee) == False:
+            print(" | SSN must be in the correct format: '123456-1234' ")
+            employee = input(" | SSN: ")
         result = self.logic.employee(0, employee, "ssn", None)
         print("""\n -----------------------------------------------------------------------------
  | -> -> Manage employee -> Search Employee                                  |
  -----------------------------------------------------------------------------""")
         for emp in result:
             print(''' | 'Employee information'                                                    |
- | Name:         {:54s}      |
- | SNN:          {:54s}      |
- | Address:      {:54s}      |
- | Home number:  {:54s}      |
- | Cell number:  {:54s}      |
- | Email:        {:54s}      |
- | Location:     {:54s}      |
- | Company role: {:54s}      |
  |                                                                           |
+ | Name:          {:54s}     |
+ | SNN:           {:54s}     |
+ | Address:       {:54s}     |
+ | Home number:   {:54s}     |
+ | Cell number:   {:54s}     |
+ | Email:         {:54s}     |
+ | Location:      {:54s}     |
+ | Company role:  {:54s}     |
  |                                                                           |
  |                                                                           |
  ----------------------------------------------------------------------------- '''.format(emp.name, emp.ssn, emp.address, emp.home_phone, emp.gsm_phone, emp.email, emp.location, emp.role))
